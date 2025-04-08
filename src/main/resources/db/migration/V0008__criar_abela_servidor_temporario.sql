fcf8eb436c8e5a17e72442ecdb77a7820a08756d
venhaserjava <venhaserjava@gmail.com>
2025-04-08 16:58:38 -0400

    feat: migrations

---
+++ src/main/resources/db/migration/V0008__criar_abela_servidor_temporario.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS servidor_temporario (
+    st_id SERIAL PRIMARY KEY,
+    pes_id BIGINT NOT NULL,
+    st_data_admissao DATE NOT NULL,
+    st_data_demissao DATE,
+    CONSTRAINT fk_servidor_temporario_pessoa FOREIGN KEY (pes_id) REFERENCES pessoa (pes_id)
+);
