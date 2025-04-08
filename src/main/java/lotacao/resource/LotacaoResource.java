4be4d09646098967a37dc65d6ae3d25ddb9bb3e1
venhaserjava <venhaserjava@gmail.com>
2025-04-08 19:34:02 -0400

    feat: Lotação - Implementado o Resource

---
+++ src/main/java/lotacao/resource/LotacaoResource.java
@@ -0,0 +1,37 @@
+package lotacao.resource;
+
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import lotacao.dtos.request.LotacaoRequest;
+import lotacao.services.LotacaoService;
+
+@Path("/lotacoes")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class LotacaoResource {
+
+    @Inject LotacaoService service;
+
+    @POST
+    public Response create(LotacaoRequest request) {
+        return Response.ok(service.create(request)).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response findById(@PathParam("id") Long id) {
+        return service.findById(id)
+                .map(Response::ok)
+                .orElse(Response.status(Response.Status.NOT_FOUND))
+                .build();
+    }
+
+    @GET
+    public Response list(@QueryParam("page") @DefaultValue("0") int page,
+                         @QueryParam("size") @DefaultValue("10") int size) {
+        return Response.ok(service.findAll(page, size)).build();
+    }
+}
