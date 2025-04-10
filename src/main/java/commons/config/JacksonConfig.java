adcb189dd635f2926e32e1c4a9f5d04735c8ebe8
venhaserjava <venhaserjava@gmail.com>
2025-04-09 21:15:11 -0400

    refactor: Servidor - endpoints de POST

--- src/main/java/commons/config/JacksonConfig.java
+++ src/main/java/commons/config/JacksonConfig.java
@@ -1,4 +1,19 @@
 package commons.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import jakarta.annotation.PostConstruct;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+
+@Singleton
 public class JacksonConfig {
+
+    @Inject
+    ObjectMapper objectMapper;
+
+    @PostConstruct
+    void init() {
+        objectMapper.registerModule(new JavaTimeModule());
+    }
 }
